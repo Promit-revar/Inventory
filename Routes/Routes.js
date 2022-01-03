fff7d8695c38548aef42674b50760d3083fcf90f
Promit-revar <61512425+Promit-revar@users.noreply.github.com>
2022-01-03 10:50:08 +0530

    filenames changed

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 app.use(express.json());                        
 app.use(express.urlencoded({extended:true}));
-const func=require('../Controllers/Insert.js');
+const func=require('../Controllers/RegisterandLoginController.js');
 app.get('/',(req,res)=>{
     res.send("<h1>Hello World!</h1>");
 });
